061da005a3bd3ab1233afbab2487b697d64fdc15
Jolene Sutton <jolene.sutton@gmail.com>
2025-10-24 10:01:00 -0300

    add example data set

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Example data set
+#'
+#' @format A data frame with 12,022 rows and 2 variables:
+#' \describe{
+#'   \item{OBJECTID}{Spatial grid cell identifier}
+#'   \item{weight}{Spatial grid cell sampling weight}
+#' }
+"sampling.weights"
